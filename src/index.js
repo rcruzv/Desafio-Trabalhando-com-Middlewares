3ef8dfb5dcd8363c1c0453ce15e3332d905cd58c
Rodrigo da Cruz Vieira <rodrigocruz.vieira@portoseguro.com.br>
2021-11-12 13:36:55 -0300

    Resolution

--- src/index.js
+++ src/index.js
@@ -10,19 +10,63 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+
+  const user = users.find((user) => user.username === username)
+  if (!user) {
+    return response.status(404).json({ error: "User not found!" })
+  }
+
+  request.user = user
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+  const { pro, todos } = user
+
+  if (!pro && (todos.length >= 3)) {
+    return response.status(403).json({ error: 'Upgrade your plan or exclude some todos!' })
+  }
+
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  //Validar o usuário
+  const user = users.find((user) => user.username === username)
+  if (!user) {
+    return response.status(404).json({ error: "User not found!" })
+  }
+
+  //Validar o uuid
+  if (!validate(id)) {
+    return response.status(400).json({ error: 'ID not valid!' })
+  }
+
+  //Validar o todo
+  const todo = user.todos.find((todo) => todo.id === id)
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not found!" })
+  }
+  request.user = user
+  request.todo = todo
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user = users.find((user) => user.id === id)
+  if (!user) {
+    return response.status(404).json({ error: 'User not found!' })
+  }
+
+  request.user = user
+  return next()
 }
 
 app.post('/users', (request, response) => {
